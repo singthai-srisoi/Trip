102868c25dc9c9cc731152f4896f6a55c2de9503
singthai-srisoi <singthaisrisoi010801@gmail.com>
2025-06-12 12:47:10 +0800

    feat: add Pikaday datepicker and implement click/tap outside directives

--- frontend/src/routes/(main)/home/+page.svelte
+++ frontend/src/routes/(main)/home/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import { fade, fly } from "svelte/transition";
 	import type { PageData } from "./$types";
+	import { clickOutside, tapOutside } from "$lib/directive/clickOutside.svelte";
+    import "cally";
 
     interface Props {
         data: {
@@ -12,22 +15,65 @@
     }: Props = $props()
 
     $inspect(data)
-    let action_modal: HTMLDialogElement
     let editing = $state(0)
+
+    let action_modal: HTMLDialogElement
     let show_action_modal = (d: number) => {
         editing = d
         action_modal.showModal()
     }
+
+    let action_drawer = $state(false)
+    let show_action_drawer = (d: number) => {
+        editing = d
+        action_drawer = true
+    }
+
+    let cally1: HTMLDivElement
 </script>
 
 
-<div class="flex flex-col gap-1.5">
+<div class="flex flex-col gap-1.5 p-1">
+    <label class="input w-full mb-3 border-1 border-gray-200 shadow-md">
+        <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <g
+            stroke-linejoin="round"
+            stroke-linecap="round"
+            stroke-width="2.5"
+            fill="none"
+            stroke="currentColor"
+            >
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+            </g>
+        </svg>
+        <input type="search" class="grow" placeholder="Search" />
+    </label>
+    <button popovertarget="cally-popover1" class="input input-border" id="cally1" style="anchor-name:--cally1">
+    Pick a date
+    </button>
+    <div popover id="cally-popover1" class="dropdown bg-base-100 rounded-box shadow-lg" style="position-anchor:--cally1"
+        bind:this={cally1}
+    >
+    <!-- <calendar-date class="cally" onchange={(e: any) => cally1.innerText = e.value}>
+        <svg aria-label="Previous" class="fill-current size-4"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.75 19.5 8.25 12l7.5-7.5"></path></svg>
+        <svg aria-label="Next" class="fill-current size-4"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="m8.25 4.5 7.5 7.5-7.5 7.5"></path></svg>
+        <calendar-month></calendar-month>
+    </calendar-date> -->
+    <calendar-range months={2}>
+        <calendar-month></calendar-month>
+        <calendar-month offset={1}></calendar-month>
+    </calendar-range>
+    </div>
 {#each data.trips as trip, i}
-    <div class="collapse collapse-arrow join-item border-base-300 border">
+    <div class="collapse collapse-arrow join-item border-1 border-gray-200 shadow-md bg-white">
         <input type="checkbox" name="my-accordion-4" />
-        <div class="collapse-title font-semibold flex items-center gap-2">{trip.vehicle.plate_no} <div class="badge badge-xs badge-primary">On Going</div></div>
-        <div class="collapse-content text-sm">
-            <div class="flex items-center justify-between px-2">
+        <div class="collapse-title font-semibold flex items-center gap-2">
+            {trip.vehicle.plate_no}
+            <div class="badge badge-xs badge-active badge-primary">On Going</div>
+        </div>
+        <div class="collapse-content text-sm pl-0.5">
+            <div class="flex items-center justify-between">
                 <ul class="timeline timeline-vertical">
                     {#each trip.points as point, n}
                         {#if n == 0}
@@ -97,8 +143,8 @@
                     </li> -->
                 </ul>
                 <div class="flex flex-col justify-between items-center gap-2 relative">
-                    <div class="timeline-middle badge badge-success badge-xs w-full">Success</div>
-                    <button class="btn btn-accent btn-soft" onclick={() => show_action_modal(trip.id)}>open modal</button>
+                    <div class="badge badge-success badge-xs badge-soft w-full ">Success</div>
+                    <button class="btn btn-soft btn-info" onclick={() => show_action_drawer(trip.id)}>Action</button>
                 </div>
 
                 
@@ -127,3 +173,28 @@
     <button>close</button>
   </form>
 </dialog>
+
+{#if action_drawer}
+<div
+    class="fixed inset-0 z-50 flex items-end bg-black/40"
+    transition:fade={{duration: 300}}
+  >
+    <div
+      class="bg-white w-full rounded-t-2xl p-6 shadow-lg min-h-[75%]"
+      transition:fly="{{ y: 300, duration: 300 }}"
+        use:tapOutside={() => {
+            action_drawer = false
+        }}
+    >
+      <h3 class="text-lg font-bold">Trip Action</h3>
+      <p class="py-4">Editing Trip ID: {editing}</p>
+      <button class="btn btn-accent mt-4" 
+        onclick={() => {
+            action_drawer = false
+            editing = 0
+        }}>
+            Close
+        </button>
+    </div>
+  </div>
+  {/if}
---
+++ frontend/src/routes/(main)/profile/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Pikaday from "pikaday";
+  let myDatepicker;
+  $effect(() => {
+    if (myDatepicker) {
+      const picker = new Pikaday({
+        field: myDatepicker
+      });
+      return () => picker.destroy();
+    }
+  });
+</script>
+
+<input type="text" class="input pika-single"  bind:this={myDatepicker} value="Pick a day" />
